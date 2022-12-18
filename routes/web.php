53987d1c43499b0e9f28d8f307b2769be5b38a0a
iArsene69 <mirzaprati@gmail.com>
2022-12-18 19:45:39 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashControl;
+use App\View\Components\layout\app;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master.master');
-});
+Route::get('/', [DashControl::class, 'index']);
