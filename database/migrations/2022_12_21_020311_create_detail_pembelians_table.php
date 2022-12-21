65aa4f0a171f9d78e0bd2186138c38f892e44057
agastyra <rangga.agastya711@gmail.com>
2022-12-21 11:35:24 +0700

    mengganti struktur table dan tipe data field nya

--- database/migrations/2022_12_21_020311_create_detail_pembelians_table.php
+++ database/migrations/2022_12_21_020311_create_detail_pembelians_table.php
@@ -15,10 +15,12 @@ class CreateDetailPembeliansTable extends Migration
     {
         Schema::create('detail_pembelians', function (Blueprint $table) {
             $table->id();
-            $table->int('transaction_id', 5);
-            $table->int('barang_id', 5);
-            $table->int('qty',3);
-            $table->double('sub_total');
+            // $table->int('transaction_id', 5); diganti oleh rangga
+            $table->foreignId('transaksi_pembelians_id');
+            $table->foreignId('barangs_id');
+            // $table->int('barang_id', 5); diganti oleh rangga
+            $table->integer('qty', 4)->default(0);
+            $table->double('sub_total')->default(0);
             $table->timestamps();
         });
     }
