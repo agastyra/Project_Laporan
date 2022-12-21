38255671a0b242fe574e6bd89e263d342773f151
agastyra <rangga.agastya711@gmail.com>
2022-12-21 09:53:08 +0700

    commit migration pembelians

--- database/migrations/2022_12_15_132103_create_transaksi_pembelians_table.php
+++ database/migrations/2022_12_15_132103_create_transaksi_pembelians_table.php
@@ -14,7 +14,6 @@ class CreateTransaksiPembeliansTable extends Migration
     public function up()
     {
         Schema::create('transaksi_pembelians', function (Blueprint $table) {
-<<<<<<< HEAD
             $table->id(); # ditambahkan oleh rangga
             $table->integer('no_transaction', 5); # direlasikan ke bukti kas keluar
             $table->date('date');
@@ -23,14 +22,6 @@ class CreateTransaksiPembeliansTable extends Migration
             $table->double('diskon')->default(0);
             $table->double('bayar')->default(0);
             $table->double('kembali')->default(0);
-=======
-            $table->id();
-            $table->integer('no_transaksi', 4);
-            $table->date('tanggal');
-            $table->string('vendor', 35);
-            $table->double('gtotal');
-            $table->double('pot_harga');
->>>>>>> 8fea5a86bf60bca47033f69e51a76ade6ccdb752
             $table->timestamps();
         });
     }
