d47b6a3253a3d4a1e1611d4e3ad69391d463c182
agastyra <rangga.agastya711@gmail.com>
2022-12-21 09:14:54 +0700

    menambahkan controller untuk model akun

--- app/Http/Controllers/akuncontroller.php
+++ app/Http/Controllers/akuncontroller.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\akun;
 
 class akuncontroller extends Controller
 {
-    //
+    public function index()
+    {
+        $akuns = akun::all();
+
+        return view('akun.index', compact('akuns'));
+    }
+
+    public function show($id)
+    {
+        $akun = Akun::find($id);
+
+        return view('akun.show', compact('akun'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\akuncontroller;
 use App\Http\Controllers\DashControl;
-use App\View\Components\layout\app;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', [DashControl::class, 'index']);
+Route::get('/accounting/accounts', [akuncontroller::class, "index"]);
