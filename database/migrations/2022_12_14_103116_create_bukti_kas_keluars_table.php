51e7a01242f8c105067d4e269cbda2eb7d8937ea
iArsene69 <mirzaprati@gmail.com>
2022-12-21 09:25:53 +0700

    mmmmmm

--- database/migrations/2022_12_14_103116_create_bukti_kas_keluars_table.php
+++ database/migrations/2022_12_14_103116_create_bukti_kas_keluars_table.php
@@ -16,7 +16,7 @@ class CreateBuktiKasKeluarsTable extends Migration
         Schema::create('bukti_kas_keluars', function (Blueprint $table) {
             $table->id();
             $table->Date('tanggal');
-            $table->foreignId('id_transaksi');
+            $table->foreignId('transaksi_pembelians_id');
             $table->string('description');
             $table->string('is_other');
             $table->string('other_account');
