d27f8f56985765554886c2540d81280cd59614e4
SuperPras4u <budhieprasetya53@gmail.com>
2022-12-22 18:25:32 +0700

    edit view

--- resources/views/akun/index.blade.php
+++ resources/views/akun/index.blade.php
@@ -9,18 +9,15 @@
                         <table class="table table-sm">
                             <thead>
                                 <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col-md">No Akun</th>
+                                    <th scope="col-md">Nama Akun</th>
+                                    <th scope="col-md">Action</th>
+
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
                                 </tr>
                             </tbody>
                         </table>
