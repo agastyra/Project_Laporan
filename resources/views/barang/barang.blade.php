95b217e9770aea84fdf3bffff8d78bc88490cb0a
SuperPras4u <budhieprasetya53@gmail.com>
2022-12-22 10:58:29 +0700

    view barang

---
+++ resources/views/barang/barang.blade.php
@@ -0,0 +1,26 @@
+<x-layout.app>
+
+    <div class="row">
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Small Table</h5>
+			  <div class="table-responsive">
+               <table class="table table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+
+</x-layout.app>
