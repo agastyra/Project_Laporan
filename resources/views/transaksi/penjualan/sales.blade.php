5616a820954df86b8f48a1b8ebd7ccacf05d162b
agastyra <rangga.agastya711@gmail.com>
2022-12-26 15:17:50 +0700

    sales

--- resources/views/transaksi/penjualan/sales.blade.php
+++ resources/views/transaksi/penjualan/sales.blade.php
@@ -6,7 +6,8 @@
                 <div class="card-body">
                     <h4 class="card-tittle"><i class="mdi mdi-magnify text-info icon-md"></i> Cari Barang</h4>
                     <div class="form-group">
-                        <select class="js-example-basic-single" style="width:100%">
+                        <select class="js-example-basic-single"
+                            style="width:100%">
                             <option value="AL">Alabama</option>
                             <option value="WY">Wyoming</option>
                             <option value="AM">America</option>
@@ -39,10 +40,14 @@
                                     <td>100.0000</td>
                                     <td>
                                         <div class="col-sm-6">
-                                            <input class="form-control text-light" type="number" name="qty" id="qty">
+                                            <input class="form-control text-light"
+                                                type="number"
+                                                name="qty"
+                                                id="qty">
                                         </div>
                                     </td>
-                                    <td><button class="btn btn-icon btn-success btn-sm" type="submit">
+                                    <td><button class="btn btn-icon btn-success btn-sm"
+                                            type="submit">
                                             <i class="mdi mdi-cart"></i></button>
                                     </td>
                                 </tr>
@@ -63,13 +68,17 @@
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Tanggal</label>
                                 <div class="col-sm-9 text-dark">
-                                    <input class="form-control text-dark" placeholder="{{ date('Y-m-d') }}" disabled />
+                                    <input class="form-control text-dark"
+                                        placeholder="{{ date('Y-m-d') }}"
+                                        disabled />
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Kasir</label>
                                 <div class="col-sm-9">
-                                    <select name="" id="" class="form-control text-light">
+                                    <select name=""
+                                        id=""
+                                        class="form-control text-light">
                                         <option value="Kasir 1">Kasir 1</option>
                                         <option value="Kasir 2">Kasir 2</option>
                                     </select>
@@ -80,13 +89,17 @@
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Grand Total</label>
                                 <div class="col-sm-9">
-                                    <input class="form-control text-dark" placeholder="0" disabled />
+                                    <input class="form-control text-dark"
+                                        placeholder="0"
+                                        disabled />
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Kembali</label>
                                 <div class="col-sm-9">
-                                    <input class="form-control text-dark" placeholder="0" disabled />
+                                    <input class="form-control text-dark"
+                                        placeholder="0"
+                                        disabled />
                                 </div>
                             </div>
                         </div>
@@ -94,12 +107,15 @@
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Bayar</label>
                                 <div class="col-sm-9">
-                                    <input class="form-control text-light" type="number" placeholder="Bayar" />
+                                    <input class="form-control text-light"
+                                        type="number"
+                                        placeholder="Bayar" />
                                     <div class="col-sm-12 mt-3">
-                                        <button type="submit" class="btn btn-success"><i
-                                                class="mdi mdi-cart-outline"></i>
+                                        <button type="submit"
+                                            class="btn btn-success"><i class="mdi mdi-cart-outline"></i>
                                             Simpan</button>
-                                        <button type="submit" class="btn btn-info"><i class="mdi mdi-fax"></i> Cetak
+                                        <button type="submit"
+                                            class="btn btn-info"><i class="mdi mdi-fax"></i> Cetak
                                             Nota</button>
                                     </div>
                                 </div>
@@ -126,11 +142,15 @@
                                             <td>1</td>
                                             <td> $ 77.99 </td>
                                             <td>
-                                                <button type="submit" class="btn btn-icon btn-success btn-sm "
-                                                    data-bs-toggle="modal" data-bs-target="#modal-edit"><i
+                                                <button type="submit"
+                                                    class="btn btn-icon btn-success btn-sm "
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#modal-edit"><i
                                                         class="mdi mdi-pencil icon-sm"></i></button>
-                                                <button type="submit" class="btn btn-icon btn-danger btn-sm"
-                                                    data-bs-toggle="modal" data-bs-target="#modal-hapus"><i
+                                                <button type="submit"
+                                                    class="btn btn-icon btn-danger btn-sm"
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#modal-hapus"><i
                                                         class="mdi mdi-delete icon-sm"></i></button>
                                             </td>
                                         </tr>
@@ -143,46 +163,69 @@
             </div>
         </div>
         <!-- Modal -->
-        <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
+        <div class="modal fade"
+            id="modal-edit"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="modalTitleId"
             aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog"
+                role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="modalTitleId">Edit Barang</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title"
+                            id="modalTitleId">Edit Barang</h5>
+                        <button type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="container-fluid">
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Nama Barang</label>
                                 <div class="col-sm-9">
-                                    <input class="form-control text-dark" disabled />
+                                    <input class="form-control text-dark"
+                                        disabled />
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label">Jumlah</label>
                                 <div class="col-sm-9">
-                                    <input class="form-control text-dark" type="number" placeholder="1" />
+                                    <input class="form-control text-dark"
+                                        type="number"
+                                        placeholder="1" />
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i
-                                class="mdi mdi-window-close"></i> Tutup</button>
-                        <button type="submit" class="btn btn-success"><i class="mdi mdi-floppy"></i> Simpan</button>
+                        <button type="button"
+                            class="btn btn-danger"
+                            data-bs-dismiss="modal"><i class="mdi mdi-window-close"></i> Tutup</button>
+                        <button type="submit"
+                            class="btn btn-success"><i class="mdi mdi-floppy"></i> Simpan</button>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="modal fade" id="modal-hapus" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
+        <div class="modal fade"
+            id="modal-hapus"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="modalTitleId"
             aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog"
+                role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="modalTitleId">Hapus Barang</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title"
+                            id="modalTitleId">Hapus Barang</h5>
+                        <button type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="container-fluid">
@@ -192,37 +235,40 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i
-                                class="mdi mdi-window-close"></i> Batal</button>
-                        <button type="submit" class="btn btn-success"><i class="mdi mdi-check"></i> Hapus</button>
+                        <button type="button"
+                            class="btn btn-danger"
+                            data-bs-dismiss="modal"><i class="mdi mdi-window-close"></i> Batal</button>
+                        <button type="submit"
+                            class="btn btn-success"><i class="mdi mdi-check"></i> Hapus</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    @push('jssj')
+        <script>
+            var modalEd = document.getElementById('modal-edit');
+
+            modalEd.addEventListener('show.bs.modal', function(event) {
+                // Button that triggered the modal
+                let button = event.relatedTarget;
+                // Extract info from data-bs-* attributes
+                let recipient = button.getAttribute('data-bs-whatever');
+
+                // Use above variables to manipulate the DOM
+            });
+        </script>
+        <script>
+            var modalHp = document.getElementById('modal-hapus');
+
+            modalHp.addEventListener('show.bs.modal', function(event) {
+                // Button that triggered the modal
+                let button = event.relatedTarget;
+                // Extract info from data-bs-* attributes
+                let recipient = button.getAttribute('data-bs-whatever');
+
+                // Use above variables to manipulate the DOM
+            });
+        </script>
+    @endpush
 </x-layout.app>
-@push('jssj')
-<script>
-    var modalEd = document.getElementById('modal-edit');
-        
-            modalEd.addEventListener('show.bs.modal', function (event) {
-                  // Button that triggered the modal
-                  let button = event.relatedTarget;
-                  // Extract info from data-bs-* attributes
-                  let recipient = button.getAttribute('data-bs-whatever');
-        
-                // Use above variables to manipulate the DOM
-            });
-</script>
-<script>
-    var modalHp = document.getElementById('modal-hapus');
-        
-            modalHp.addEventListener('show.bs.modal', function (event) {
-                  // Button that triggered the modal
-                  let button = event.relatedTarget;
-                  // Extract info from data-bs-* attributes
-                  let recipient = button.getAttribute('data-bs-whatever');
-        
-                // Use above variables to manipulate the DOM
-            });
-</script>
-@endpush
