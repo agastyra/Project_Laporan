a19ad105424eb683635489594fd19dd25b5cb239
agastyra <rangga.agastya711@gmail.com>
2022-12-27 12:33:03 +0700

    format document

--- app/Http/Controllers/bukti_kas_keluarcontroller.php
+++ app/Http/Controllers/bukti_kas_keluarcontroller.php
@@ -2,16 +2,10 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Models\bukti_kas_keluar;
-use Illuminate\Http\Request;
-
 class bukti_kas_keluarcontroller extends Controller
 {
     public function index()
     {
-
-
         return view('BuktiKasKeluar.index');
     }
 }
