c5d85753e7482b1be2da172a3f00501f8a8e52c4
superpras4u <budhieprasetya53@gmail.com>
2023-01-31 16:04:35 +0700

    sidebar config

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -70,83 +70,47 @@
                     <span class="menu-title">Dashboard</span>
                 </a>
             </li>
+
             <li class="nav-item menu-items">
-                <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                    aria-controls="ui-basic">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-laptop"></i>
-                    </span>
-                    <span class="menu-title">Basic UI Elements</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="ui-basic">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
-                        </li>
-                        <li class="nav-item"> <a class="nav-link"
-                                href="pages/ui-features/typography.html">Typography</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/forms/basic_elements.html">
+                <a class="nav-link" href="{{Route('accounts')}}">
                     <span class="menu-icon">
                         <i class="mdi mdi-playlist-play"></i>
                     </span>
-                    <span class="menu-title">Form Elements</span>
+                    <span class="menu-title">Akun</span>
                 </a>
             </li>
             <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/tables/basic-table.html">
+                <a class="nav-link" href="{{Route('barang')}}">
                     <span class="menu-icon">
                         <i class="mdi mdi-table-large"></i>
                     </span>
-                    <span class="menu-title">Tables</span>
+                    <span class="menu-title">Barang</span>
                 </a>
             </li>
             <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/charts/chartjs.html">
+                <a class="nav-link" href="penyesuaian">
                     <span class="menu-icon">
                         <i class="mdi mdi-chart-bar"></i>
                     </span>
-                    <span class="menu-title">Charts</span>
+                    <span class="menu-title">Jurnal Penyesuaian</span>
                 </a>
             </li>
             <li class="nav-item menu-items">
-                <a class="nav-link" href="pages/icons/mdi.html">
+                <a class="nav-link" href="memo">
                     <span class="menu-icon">
                         <i class="mdi mdi-contacts"></i>
                     </span>
-                    <span class="menu-title">Icons</span>
+                    <span class="menu-title">Jurnal Memorial</span>
                 </a>
             </li>
             <li class="nav-item menu-items">
-                <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                    <span class="menu-icon">
-                        <i class="mdi mdi-security"></i>
-                    </span>
-                    <span class="menu-title">User Pages</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="auth">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a>
-                        </li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item menu-items">
-      
-          <a class="nav-link" href="documentation">
+
+                <a class="nav-link" href="documentation">
                     <span class="menu-icon">
                         <i class="mdi mdi-file-document-box"></i>
                     </span>
-                    <span class="menu-title">Documentation</span>
+      
+              <span class="menu-title">Bukti</span>
                 </a>
             </li>
         </ul>
