cb5490fa41f849622f9d9bc1a6948f937cef90e5
agastyra <rangga.agastya711@gmail.com>
2023-02-15 15:26:01 +0700

    ganti url

--- routes/web.php
+++ routes/web.php
@@ -55,9 +55,9 @@ Route::put('/purchase/update_detail_qty', [transaksi_pembeliancontroller::class,
 Route::delete('/purchase/delete_detail', [transaksi_pembeliancontroller::class, "delete_detail"])->name('delete_detail');
 
 //route kas keluar
-Route::get('/accounting/cash_out', [bukti_kas_keluarcontroller::class, "index"])->name('cash_out');
-Route::post('/accounting/cash_out', [bukti_kas_keluarcontroller::class, "save"])->name('save_cash_out');
-Route::get('/accounting/cash_out/new', [bukti_kas_keluarcontroller::class, "form"])->name('create_cash_out');
+Route::get('/accounting/cash-out', [bukti_kas_keluarcontroller::class, "index"])->name('cash_out');
+Route::post('/accounting/cash-out', [bukti_kas_keluarcontroller::class, "save"])->name('save_cash_out');
+Route::get('/accounting/cash-out/new', [bukti_kas_keluarcontroller::class, "form"])->name('create_cash_out');
 Route::get('/jurnal_kas_keluar', [bukti_kas_keluarcontroller::class, "jurnal"]);
 Route::get('/nota_pembelian', [bukti_kas_keluarcontroller::class, 'nota']);
 
