6dbac6a089186803cbf12231d675d5cd98403c4f
agastyra <rangga.agastya711@gmail.com>
2023-03-05 00:36:39 +0700

    mengambil data dari jurnal memorial dan jurnal penyesuaian

--- app/Http/Livewire/BukuBesar.php
+++ app/Http/Livewire/BukuBesar.php
@@ -12,7 +12,31 @@ class BukuBesar extends Component
 
     public function render()
     {
-        $buku_besars =
+        $jurnal_memorial = akun::join('jurnal_memorial_details', 'akuns.id', '=', 'jurnal_memorial_details.akun_id')
+        ->join('jurnal_memorials', 'jurnal_memorials.id', '=', 'jurnal_memorial_details.jurnal_memorial_id')
+        ->select(
+            'jurnal_memorials.id as id',
+            'jurnal_memorials.date as tanggal',
+            'akuns.id as akun_id',
+            'akuns.no_account as no_account',
+            'akuns.name_account as name_account',
+            'jurnal_memorial_details.debet as debet',
+            'jurnal_memorial_details.kredit as kredit'
+        )->get();
+
+        $jurnal_penyesuaian = akun::join('jurnal_penyesuaian_details', 'akuns.id', '=', 'jurnal_penyesuaian_details.akun_id')
+        ->join('jurnal_penyesuaians', 'jurnal_penyesuaians.id', '=', 'jurnal_penyesuaians.jurnal_penyesuaian_id')
+        ->select(
+            'jurnal_penyesuaians.id as id',
+            'jurnal_penyesuaians.date as tanggal',
+            'akuns.id as akun_id',
+            'akuns.no_account as no_account',
+            'akuns.name_account as name_account',
+            'jurnal_penyesuaian_details.debet as debet',
+            'jurnal_penyesuaian_details.kredit as kredit'
+        )->get();
+
+        dd($jurnal_memorial . $jurnal_penyesuaian);
 
         $accounts = akun::orderBy('no_account', 'asc')->get();
         return view('livewire.buku-besar', [
