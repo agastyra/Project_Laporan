7ff830a910d6c56af33b6957c4934e8976aec55a
agastyra <rangga.agastya711@gmail.com>
2023-03-07 02:07:30 +0700

    filter date buku besar

--- app/Http/Livewire/BukuBesar.php
+++ app/Http/Livewire/BukuBesar.php
@@ -10,54 +10,112 @@ class BukuBesar extends Component
 {
     public $selectedAkun = '';
     public $akun = '';
-    public $query = '';
+    public $tanggal_awal = '';
+    public $tanggal_akhir = '';
+    public $query = [];
+
+    protected $rules = [
+        'selectedAkun' => 'required',
+        'tanggal_awal' => 'required',
+        'tanggal_akhir' => 'required',
+    ];
 
     public function mount()
     {
-        $this->query = "SELECT `id`, `date`, `keterangan`, `akun_id`, `no_account`, `name_account`,
-                            `CASE
-                                WHEN `debet` = 0 THEN '-'
-                                ELSE `debet`
-                            END AS debet,
-                            CASE
-                                WHEN `kredit` = 0 THEN '-'
-                                ELSE `kredit`
-                            END AS kredit,
-                            CASE
-                                WHEN `type_account` = 1 OR `type_account` = 3
-                                    THEN SUM(`debet` - `kredit`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
-                                ELSE SUM(`kredit` - `debet`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
-                            END AS saldo_akhir
-                        FROM (
-                        SELECT jm.`id`, jm.`date`, 'Jurnal Umum' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
-                            jmd.`debet`, jmd.`kredit`
-                            , jm.`created_at`
-                        FROM `jurnal_memorials` jm
-                        JOIN `jurnal_memorial_details` jmd ON jm.id = jmd.`jurnal_memorial_id`
-                        JOIN `akuns` ON jmd.`akun_id` = `akuns`.`id`
+        if (request('akun_id') && request('tanggal_awal_filter') && request('tanggal_akhir_filter')) {
+            $akun = akun::where('id', request('akun_id'))->first();
+            $this->akun = "( $akun->no_account ) $akun->name_account";
 
-                        UNION ALL
+            $query = "SELECT `id`, `date`, `keterangan`, `akun_id`, `no_account`, `name_account`,
+                                CASE
+                                    WHEN `debet` = 0 THEN '-'
+                                    ELSE `debet`
+                                END AS debet,
+                                CASE
+                                    WHEN `kredit` = 0 THEN '-'
+                                    ELSE `kredit`
+                                END AS kredit,
+                                CASE
+                                    WHEN `type_account` = 1 OR `type_account` = 3
+                                        THEN SUM(`debet` - `kredit`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
+                                    ELSE SUM(`kredit` - `debet`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
+                                END AS saldo_akhir
+                            FROM (
+                            SELECT jm.`id`, jm.`date`, 'Jurnal Umum' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
+                                jmd.`debet`, jmd.`kredit`
+                                , jm.`created_at`
+                            FROM `jurnal_memorials` jm
+                            JOIN `jurnal_memorial_details` jmd ON jm.id = jmd.`jurnal_memorial_id`
+                            JOIN `akuns` ON jmd.`akun_id` = `akuns`.`id`
 
-                        SELECT jp.`id`, jp.`date`, 'Jurnal Penyesuaian' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
-                            jpd.`debet`, jpd.`kredit`
-                            , jp.`created_at`
-                        FROM `jurnal_penyesuaians` jp
-                        JOIN `jurnal_penyesuaian_details` jpd ON jp.id = jpd.`jurnal_penyesuaian_id`
-                        JOIN `akuns` ON jpd.`akun_id` = `akuns`.`id`
-                        ) AS buku_besar
-                        ORDER BY `date`, `created_at` ASC`
-                    ";
+                            UNION ALL
+
+                            SELECT jp.`id`, jp.`date`, 'Jurnal Penyesuaian' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
+                                jpd.`debet`, jpd.`kredit`
+                                , jp.`created_at`
+                            FROM `jurnal_penyesuaians` jp
+                            JOIN `jurnal_penyesuaian_details` jpd ON jp.id = jpd.`jurnal_penyesuaian_id`
+                            JOIN `akuns` ON jpd.`akun_id` = `akuns`.`id`
+                            ) AS buku_besar
+                            WHERE `akun_id` = " . request('akun_id') . " AND
+                            `date` BETWEEN '" . request('tanggal_awal_filter') . "'
+                            AND '" . request('tanggal_akhir_filter') . "'
+                            ORDER BY `date`, `created_at` ASC
+                        ";
+            $this->query = DB::select($query);
+        } else if (request('akun_id') || (request('tanggal_awal_filter') && request('tanggal_akhir_filter'))) {
+            $akun = akun::where('id', request('akun_id'))->first();
+            $this->akun = "( $akun->no_account ) $akun->name_account";
+
+            $query = "SELECT `id`, `date`, `keterangan`, `akun_id`, `no_account`, `name_account`,
+                                CASE
+                                    WHEN `debet` = 0 THEN '-'
+                                    ELSE `debet`
+                                END AS debet,
+                                CASE
+                                    WHEN `kredit` = 0 THEN '-'
+                                    ELSE `kredit`
+                                END AS kredit,
+                                CASE
+                                    WHEN `type_account` = 1 OR `type_account` = 3
+                                        THEN SUM(`debet` - `kredit`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
+                                    ELSE SUM(`kredit` - `debet`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
+                                END AS saldo_akhir
+                            FROM (
+                            SELECT jm.`id`, jm.`date`, 'Jurnal Umum' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
+                                jmd.`debet`, jmd.`kredit`
+                                , jm.`created_at`
+                            FROM `jurnal_memorials` jm
+                            JOIN `jurnal_memorial_details` jmd ON jm.id = jmd.`jurnal_memorial_id`
+                            JOIN `akuns` ON jmd.`akun_id` = `akuns`.`id`
+
+                            UNION ALL
+
+                            SELECT jp.`id`, jp.`date`, 'Jurnal Penyesuaian' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
+                                jpd.`debet`, jpd.`kredit`
+                                , jp.`created_at`
+                            FROM `jurnal_penyesuaians` jp
+                            JOIN `jurnal_penyesuaian_details` jpd ON jp.id = jpd.`jurnal_penyesuaian_id`
+                            JOIN `akuns` ON jpd.`akun_id` = `akuns`.`id`
+                            ) AS buku_besar
+                            WHERE `akun_id` = " . request('akun_id') . "
+                            ORDER BY `date`, `created_at` ASC
+                        ";
+            $this->query = DB::select($query);
+        } else {
+            $this->akun = "Silahkan pilih akun terlebih dahulu!";
+        }
     }
 
     public function render()
     {
         $accounts = akun::orderBy('no_account', 'asc')->get();
 
-        $buku_besar = DB::select($this->query);
+        $query = $this->query;
 
         return view('livewire.buku-besar', [
             'accounts' => $accounts,
-            'buku_besar' => $buku_besar,
+            'query' => $query,
         ])->layout('components.layout.app');
     }
 
@@ -67,7 +125,7 @@ class BukuBesar extends Component
             $akun = akun::where('id', $value)->first();
             $this->akun = "( $akun->no_account ) $akun->name_account";
 
-            $this->query = "SELECT `id`, `date`, `keterangan`, `akun_id`, `no_account`, `name_account`,
+            $query = "SELECT `id`, `date`, `keterangan`, `akun_id`, `no_account`, `name_account`,
                                 CASE
                                     WHEN `debet` = 0 THEN '-'
                                     ELSE `debet`
@@ -101,42 +159,11 @@ class BukuBesar extends Component
                             WHERE `akun_id` = $value
                             ORDER BY `date`, `created_at` ASC
                         ";
+            $this->query = DB::select($query);
 
         } else {
             $this->akun = '';
-            $this->query = "SELECT `id`, `date`, `keterangan`, `akun_id`, `no_account`, `name_account`,
-                                CASE
-                                    WHEN `debet` = 0 THEN '-'
-                                    ELSE `debet`
-                                END AS debet,
-                                CASE
-                                    WHEN `kredit` = 0 THEN '-'
-                                    ELSE `kredit`
-                                END AS kredit,
-                                CASE
-                                    WHEN `type_account` = 1 OR `type_account` = 3
-                                        THEN SUM(`debet` - `kredit`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
-                                    ELSE SUM(`kredit` - `debet`) OVER (PARTITION BY `akun_id` ORDER BY `date`, `created_at`)
-                                END AS saldo_akhir
-                            FROM (
-                            SELECT jm.`id`, jm.`date`, 'Jurnal Umum' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
-                                jmd.`debet`, jmd.`kredit`
-                                , jm.`created_at`
-                            FROM `jurnal_memorials` jm
-                            JOIN `jurnal_memorial_details` jmd ON jm.id = jmd.`jurnal_memorial_id`
-                            JOIN `akuns` ON jmd.`akun_id` = `akuns`.`id`
-
-                            UNION ALL
-
-                            SELECT jp.`id`, jp.`date`, 'Jurnal Penyesuaian' AS `keterangan`, `akuns`.id AS akun_id, `akuns`.`no_account`, `akuns`.`name_account`, akuns.`type_account`,
-                                jpd.`debet`, jpd.`kredit`
-                                , jp.`created_at`
-                            FROM `jurnal_penyesuaians` jp
-                            JOIN `jurnal_penyesuaian_details` jpd ON jp.id = jpd.`jurnal_penyesuaian_id`
-                            JOIN `akuns` ON jpd.`akun_id` = `akuns`.`id`
-                            ) AS buku_besar
-                            ORDER BY `date`, `created_at` ASC
-                        ";
+            $this->query = [];
         }
     }
 }
--- resources/views/livewire/buku-besar.blade.php
+++ resources/views/livewire/buku-besar.blade.php
@@ -7,69 +7,127 @@
                         <i class="mdi mdi-table-large text-danger"></i>
                         Buku Besar
                     </h4>
-                    <div class="row justify-content-start">
-                        <div class="col-md-1 d-flex flex-column">
-                            <h5 class="my-auto">Akun : </h5>
-                        </div>
-                        <div class="col-md-11">
-                            <select class="form-select form-control text-light"
-                                wire:model="selectedAkun"
-                                id="akun">
-                                <option value="">-</option>
-                                @forelse ($accounts as $account)
-                                    <option value="{{ $account->id }}">( {{ $account->no_account }} )
-                                        {{ $account->name_account }}</option>
-                                @empty
-                                    <option value="">-- Tidak ada data --</option>
-                                @endforelse
-                            </select>
-                        </div>
+                    <div align="left">
+                        <button type="button"
+                            id="filter_button"
+                            class="btn btn-inverse-info btn-icon mb-3">
+                            <i class="mdi mdi-filter-outline"></i>
+                        </button>
+                        <a href="/accounting/memorial/print_memorial?tanggal_awal_filter={{ request('tanggal_awal_filter') }}&tanggal_akhir_filter={{ request('tanggal_akhir_filter') }}"
+                            target="_blank"
+                            id="print_buku_besar"
+                            class="btn btn-inverse-primary btn-icon mb-3 d-inline-flex">
+                            <i class="mdi mdi-printer m-auto"></i>
+                        </a>
                     </div>
-                    <div class="table-responsive mt-4">
-                        <h5>{{ $akun }}</h5>
-                        <table class="table">
-                            <thead>
+                    <form id="form_filter_buku_besar"
+                        action="{{ route('ledger') }}">
+                        <div class="row justify-content-start my-4">
+                            <div class="col-md-1 d-flex flex-column">
+                                <h5 class="my-auto">Akun : </h5>
+                            </div>
+                            <div class="col-md-11">
+                                <select class="form-select form-control text-light"
+                                    wire:model.lazy="selectedAkun"
+                                    name="akun_id"
+                                    id="akun">
+                                    <option value="">-</option>
+                                    @forelse ($accounts as $account)
+                                        <option value="{{ $account->id }}">( {{ $account->no_account }} )
+                                            {{ $account->name_account }}</option>
+                                    @empty
+                                        <option value="">-- Tidak ada data --</option>
+                                    @endforelse
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row justify-content-start my-4"
+                            id="filter_buku_besar">
+                            <h5 class="mb-3">Filter :</h5>
+                            <div class="col">
+                                <div class="col-md-4 d-flex flex-column">
+                                    <h6 class="my-auto">Tanggal awal : </h5>
+                                </div>
+                                <div class="col my-3">
+                                    <input type="date"
+                                        name="tanggal_awal_filter"
+                                        id="tanggal_awal_filter"
+                                        class="form-control text-light">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-md-4 d-flex flex-column">
+                                    <h6 class="my-auto">Tanggal akhir : </h6>
+                                </div>
+                                <div class="col my-3">
+                                    <input type="date"
+                                        name="tanggal_akhir_filter"
+                                        id="tanggal_akhir_filter"
+                                        class="form-control text-light">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <a href="{{ route('ledger') }}"
+                                    class="btn btn-inverse-danger btn-icon my-auto d-inline-flex">
+                                    <i class="mdi mdi-backup-restore m-auto"></i>
+                                </a>
+                                <button type="submit"
+                                    class="btn btn-inverse-success btn-icon my-auto">
+                                    <i class="mdi mdi-magnify"></i>
+                                </button>
+                            </div>
+                    </form>
+                </div>
+                <div class="table-responsive mt-4">
+                    <h5 class="italic">{{ $akun }}</h5>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col-md">Tanggal</th>
+                                <th scope="col-md">Keterangan</th>
+                                <th scope="col-md">Ref</th>
+                                <th scope="col-md">Debet</th>
+                                <th scope="col-md">Kredit</th>
+                                <th scope="col-md">Saldo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($query as $buku_besar)
                                 <tr>
-                                    <th scope="col-md">Tanggal</th>
-                                    <th scope="col-md">Keterangan</th>
-                                    <th scope="col-md">Ref</th>
-                                    <th scope="col-md">Debet</th>
-                                    <th scope="col-md">Kredit</th>
-                                    <th scope="col-md">Saldo</th>
+                                    <td>{{ $buku_besar->date }}</td>
+                                    <td>{{ $buku_besar->keterangan }}</td>
+                                    <td>{{ $buku_besar->no_account }}</td>
+                                    <td>
+                                        @if ($buku_besar->debet == '-')
+                                            -
+                                        @else
+                                            Rp. {{ number_format($buku_besar->debet, 0, ',', '.') }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($buku_besar->kredit == '-')
+                                            -
+                                        @else
+                                            Rp. {{ number_format($buku_besar->kredit, 0, ',', '.') }}
+                                        @endif
+                                    </td>
+                                    <td>Rp. {{ number_format($buku_besar->saldo_akhir, 0, ',', '.') }}</td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($buku_besar as $bb)
-                                    <tr>
-                                        <td>{{ $bb->date }}</td>
-                                        <td>{{ $bb->keterangan }}</td>
-                                        <td>{{ $bb->no_account }}</td>
-                                        <td>
-                                            @if ($bb->debet == '-')
-                                                -
-                                            @else
-                                                Rp. {{ number_format($bb->debet, 0, ',', '.') }}
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if ($bb->kredit == '-')
-                                                -
-                                            @else
-                                                Rp. {{ number_format($bb->kredit, 0, ',', '.') }}
-                                            @endif
-                                        </td>
-                                        <td>Rp. {{ number_format($bb->saldo_akhir, 0, ',', '.') }}</td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="5">Tidak ada data</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div>
+                            @empty
+                                <tr>
+                                    <td colspan="6">Tidak ada data</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+@push('jssj')
+    <script src='{{ asset('assets/js/buku_besar/index.js') }}'></script>
+@endpush
+
 </div>
